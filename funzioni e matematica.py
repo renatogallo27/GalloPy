04175de9b3d2a57f3c2ef68573fcdf983a43471c
renatogallo27 <74650866+renatogallo27@users.noreply.github.com>
2020-11-30 18:38:03 +0100

    Add files via upload

---
+++ funzioni e matematica.py
@@ -0,0 +1,27 @@
+
+#Primo esercizio
+print("risolvo equazione (ax+b=0)")
+a=int(input("valore di a: "))
+b=int(input("valore di b: "))
+print (a,"x","+",b,"=",0)
+x=-b/a
+print("x=",x)
+print()
+
+
+#Secondo esercizio
+c=int(input("valore di c: "))
+print("restituisco il valore più grande dei 3 valori dati")
+valore_maggiore=max(a,b,c)
+print("ll numero più grande è",valore_maggiore)
+print()
+
+
+#Terzo esercizio
+print("inverto il valore delle due variabili")
+a,b=b,a
+print("a=",a)
+print("b=",b)
+
+
+        
