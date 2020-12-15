92e64318cf5d5247bf0963172c4d90fab991c227
renatogallo27 <74650866+renatogallo27@users.noreply.github.com>
2020-12-15 16:12:41 +0100

    Add files via upload

---
+++ matplotlib1.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+plt.title(label="Grafico spazio tempo di un moto")
+plt.xlabel("tempo(s)")
+plt.ylabel("spazio(m)")
+
+plt.plot([0, 1, 2, 3], [0, 5, 5, 2],)
+
+
+plt.show()
+
+
+
+
