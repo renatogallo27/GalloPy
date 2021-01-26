9e62377ebcab84747645c9aeadb480ffad068aff
renatogallo27 <74650866+renatogallo27@users.noreply.github.com>
2021-01-26 16:59:35 +0100

    Add files via upload

---
+++ Matematica/elaborazione dati statistici.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+import csv
+with open("DatiStatistici-2018.csv", "r") as filecsv:
+    lettore=csv.reader(filecsv, delimiter=";")
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
