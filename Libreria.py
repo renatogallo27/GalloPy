5e2f75681ea8275a083405c8e1f568daaeec5ec7
renatogallo27 <74650866+renatogallo27@users.noreply.github.com>
2021-03-02 17:23:24 +0100

    Add files via upload

---
+++ Libreria.py
@@ -0,0 +1,16 @@
+import json
+libreria={
+    "libro1":'{"titolo":"Harry Potter e la pietra filosofale","autore":"J. K. Rowling","genere":"Romanzo, Letteratura per ragazzi, Narrativa fantasy, Fantasy epico"}',
+    "libro2":'{"titolo":"Limonov","autore":"Emmanuel Carrère","genere":"Narrativa"}',
+    "libro3":'{"titolo":"Americanah ","autore":"Chimamanda Ngozi Adichie","genere":"Romanzo, Narrativa, Romanzo rosa"}',
+    "libro4":'{"titolo":"Fu sera e fu mattina","autore":"Ken Follett","genere":"Fiction storica, Suspense, Saga, Romanzo rosa storico"}',
+    "libro5":'{"titolo":"La vita in tempo di pace","autore":"Francesco Pecoraro","genere":"Narrativa letteraria"}',
+    "libro6":'{"titolo":"Il gigante sepolto","autore":"Kazuo Ishiguro","genere":"Romanzo, Narrativa fantasy, Fiction storica, Fantasy epico"}',
+    "libro7":'{"titolo":"La ferrovia sotterranea","autore":"Colson Whitehead","genere":"Romanzo, Fiction storica"}',
+    "libro8":'{"titolo":"Persone normali","autore":"Sally Rooney","genere":"Romanzo, Narrativa letteraria"}',
+    "libro9":'{"titolo":"I testamenti","autore":"Margaret Atwood","genere":"Fantascienza, Narrativa distopica, Fantapolitica"}',
+    "libro10":'{"titolo":"Air. La storia di Michael Jordan","autore":"David Halberstam","genere":"Biografia"}'
+}
+
+libri=json.dumps(libreria)
+
