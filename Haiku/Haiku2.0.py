4de9273229c6189c9767fc6c6a8109a471ba0aaa
renatogallo27 <74650866+renatogallo27@users.noreply.github.com>
2021-05-11 17:18:01 +0200

    Update Haiku2.0.py

--- Haiku/Haiku2.0.py
+++ Haiku/Haiku2.0.py
@@ -8,9 +8,6 @@ def genera_haiku():
     haiku1=random.choice(f.readlines())
     haiku2=random.choice(f2.readlines())
     haiku3=random.choice(f3.readlines())
-    #testo1=Text(app, text=haiku1)
-    #testo2=Text(app, text=haiku2)
-    #testo3=Text(app, text=haiku3)
     listbox= ListBox(app, width=125, height=55, items=[haiku1, haiku2, haiku3])
                                                            
 app=App(title="Generatore di Haiku")
